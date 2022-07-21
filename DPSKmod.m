7acabca588d2320737d0499b2eb186c492109538
Charan K S <90633234+charcode123@users.noreply.github.com>
2022-07-21 21:04:34 +0530

    commit

--- DPSKmod.m
+++ DPSKmod.m
@@ -10,7 +10,7 @@ function DPSK = DPSKmod(Nb,Fc,bk,Nsps,t)
     NRZ_W=reshape(Pulse*NRZ,1,[]);
     dk_W=reshape(Pulse*dk,1,[]);
     %plot(t,dk_W);
-    %plot(t,NRZ_W)
+    %plot(t,NRZ_W);
     C=sin(2*pi*Fc*t);
     DPSK=NRZ_W.*C;
 end
